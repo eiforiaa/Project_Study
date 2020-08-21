968c3cc3e5db932c21f203a035fb2e9003d78f24
Tanya Grunicheva <eiforiaa.2010@mail.ru>
2020-08-21 14:18:38 +0300

    добавлен файлик с js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// alert('Привет, Мир!'); /*это предупреждение*/
+/* создает переменную иногда это let*/
+var a = 5;
+var b = 'Hello';
+var c = true;
+var d = 5;
+// alert(a);
+
+// a = prompt ('Напишите значение переменной')
+
+// if (a == 7) {
+// 	alert ('Все верно');
+// } else {
+// 	alert('что-то другое');
+// }
+var obj = {
+	'key' : 'value',
+	'key' : 'value',
+	'key' : 'value',
+	'key' : 'value'
+}
+var user = {
+	'name': = 'Петя',
+	'surname':='abc'
+}
+alert ( user.name)
+var mas = ['Петя', 'Васечкин', 25];
+alert (mas[0])
+function doMagic (a,b) {
+	alert (a+b);
+}
+doMagic(5,6);
